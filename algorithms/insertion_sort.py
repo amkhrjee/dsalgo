8ed3df475918404ea12392f6d402fbb7f3f80db6
amkhrjee <aniruddha.mukherji@outlook.com>
2024-07-26 12:41:23 +0530

    add: insertion sort

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,17 @@
+import random
+
+
+# Inspired by Jon Bentley's pseudocode in Programming Pearls
+# https://books.google.co.in/books?id=kse_7qbWbjsC&pg=PA116&redir_esc=y#v=onepage&q&f=false
+def insertion_sort(input):
+    for i in range(len(input)):
+        for j in range(i, 0, -1):
+            if input[j - 1] > input[j]:
+                input[j - 1], input[j] = input[j], input[j - 1]
+
+
+def test_():
+    arr = [random.randint(0, 100) for _ in range(10)]
+    sorted_arr = sorted(arr)
+    insertion_sort(arr)
+    assert sorted_arr == arr
