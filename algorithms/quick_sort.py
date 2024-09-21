025ea1f96ed3d568b8df60366d6b4672a489dd71
amkhrjee <aniruddha.mukherji@outlook.com>
2024-09-21 18:35:54 +0530

    add: helful comments

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -3,16 +3,26 @@ import random
 
 def quick_sort(input):
     def partition(input, low, high):
+        # we choose the element in the last index
+        # as our pivot
         pivot = high
         # weak sort till the pivot
         index = low - 1
         for i in range(low, high):
+            # we stack the elements smaller than the pivot
+            # to the beginning of the array
             if input[i] <= input[pivot]:
                 index += 1
                 input[index], input[i] = input[i], input[index]
+        # finally we put the index on the
+        # 'top of the stack' of the numbers elements
+        # smaller than it
         index += 1
         input[index], input[pivot] = input[pivot], input[index]
+        # this automatically makes the elements larger than
+        # the pivot being placed on the right side of the pivot
         return index
+        # we return the position of the pivot
 
     def sort(input, low, high):
         if low >= high:
